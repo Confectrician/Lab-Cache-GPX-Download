b4971cbd9db89795805ea14b8f2a6cc2ea87004a
Confectrician <TheViking@ogame.de>
2015-03-16 17:25:30 +0100

    Added Comments and Description

--- labCacheDownloader.user.js
+++ labCacheDownloader.user.js
@@ -2,7 +2,7 @@
 // @name        Lab GPX Download
 // @author      Jerome Luckenbach
 // @namespace   confectrician.de
-// @description Adds a Download
+// @description Adds a Download Button to Geocaching Lab Pages to easily downlad the gpx files
 // @include     https://labs.geocaching.com/Adventures/Details/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @version     1.0
@@ -10,20 +10,27 @@
 // ==/UserScript==
 
 $(function(){
-    //        /Adventures/Details/90ced6d4-0a22-4c19-a491-7ae17d489c60
+    /*
+     * Get current Path and extract Lab guid
+     */
     pathName = window.location.pathname;
     pathValues = pathName.split("/");
     downloadPath = "/Adventures/DetailsAsGPX/" + pathValues[3];
 
-
+    /*
+     * Move existing Link to the Left
+     */
     $('#leaderboard').css({
         "margin-right" : "0px",
         "padding-right": "9px",
         "padding-left" : "9px",
         "right"        : "206px"
     });
+
+    /*
+     * Create Download Link
+     */
     $('#leaderboard').parent().append(
         '<a id="dl_link" class="link-leaderboard" href="' + downloadPath + '" style="padding-left: 9px;">Download‌ as‌ GPX‌ File</a>'
     );
-
 });
