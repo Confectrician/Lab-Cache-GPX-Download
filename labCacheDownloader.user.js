957cfc01139044b6d7ad6a585f14ef6ae953b350
Confectrician <TheViking@ogame.de>
2015-03-17 23:05:58 +0100

    Improved Style and added a Update URL

--- labCacheDownloader.user.js
+++ labCacheDownloader.user.js
@@ -5,20 +5,30 @@
 // @description Adds a Download Button to Geocaching Lab Pages to easily downlad the gpx files
 // @include     https://labs.geocaching.com/Adventures/Details/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
-// @version     1.0
+// @updateURL   https://greasyfork.org/scripts/8624-lab-gpx-download/code/Lab%20GPX%20Download.user.js
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
 $(function(){
     /*
-     * Get current Path and extract Lab guid
+     * removing -> background-image: -moz-linear-gradient(left center , rgba(157, 178, 81, 0) 0%, #9db251 100%);
+     * This gets a clearer view, if more than one Navigation Button is Displayed
+     */
+    document.styleSheets[1].cssRules[384].style['background-image'] = "none";
+
+    /*
+     * Example Path for Site      /Adventures/Details/90ced6d4-0a22-4c19-a491-7ae17d489c60
+     * Example Path for Download  /Adventures/DetailsAsGPX/90ced6d4-0a22-4c19-a491-7ae17d489c60
+     *
+     * Get current Path with GUID and create download Path
      */
     pathName = window.location.pathname;
     pathValues = pathName.split("/");
     downloadPath = "/Adventures/DetailsAsGPX/" + pathValues[3];
 
     /*
-     * Move existing Link to the Left
+     * Move existing Leaderboard Button to the left
      */
     $('#leaderboard').css({
         "margin-right" : "0px",
@@ -28,7 +38,7 @@ $(function(){
     });
 
     /*
-     * Create Download Link
+     * Create new Button with Download Link
      */
     $('#leaderboard').parent().append(
         '<a id="dl_link" class="link-leaderboard" href="' + downloadPath + '" style="padding-left: 9px;">Download‌ as‌ GPX‌ File</a>'
